8d6f7d8ae42ef6e7171738b36065b4396518ad50
Camille <cssaillard@gmail.com>
2023-12-31 10:40:47 -0400

    style home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,8 +4,15 @@ import Nous from "../../assets/IMG_1272.jpeg";
 const Home = () => {
   return (
     <Container>
-      <div>Home</div>
-      <div className="user-container"></div>
+      <div class="main-text">
+        <h1 className="container">
+          <div class="title">Bienvenue sur le site de notre mariage</div>
+          <div className="date">Le 13 juillet 2024</div>
+          <div className="lieu">
+            Au Manoir des Prévanches - 27120 Boisset les Prévanches{" "}
+          </div>
+        </h1>
+      </div>
       <div class="part-link">
         <a
           class="more-link"
@@ -29,11 +36,47 @@ const Container = styled.div`
   background-repeat: no-repeat;
   background-position: center;
   display: flex;
-  justify-content: flex-end;
+  justify-content: center;
   flex-direction: column;
+
+  .main-text {
+    color: #f08c79;
+
+    display: flex;
+    justify-content: flex-end;
+    text-align: center;
+
+    .container {
+      background-color: #ffffffc4;
+      padding: 20px;
+      border-radius: 20px;
+
+      .title {
+        color: #737171;
+        font-size: 45px;
+        padding-bottom: 20px;
+      }
+
+      .date {
+        padding-bottom: 20px;
+        font-size: 70px;
+      }
+
+      .lieu {
+        color: #737171;
+        font-size: 40px;
+      }
+    }
+  }
 
   .more-link {
     background-color: #ffffffc4;
+  }
+
+  .part-link {
+    position: absolute;
+    width: 100%;
+    bottom: 10px;
   }
 `;
 
